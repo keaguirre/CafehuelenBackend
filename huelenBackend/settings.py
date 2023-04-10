a80964d122666669f21d20d6759964bd7229ad59
keaguirre <ke.aguirre@duocuc.cl>
2023-04-09 22:05:14 -0400

    commit 09/04/22 Agregada bd online postgreSQL

--- huelenBackend/settings.py
+++ huelenBackend/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = 'django-insecure-sh-*i&7p@5-5t8(ra84jedxjd4$b4wk*6*)o8pp_r+093fw=4u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -79,13 +82,13 @@ WSGI_APPLICATION = 'huelenBackend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+    default='postgres://cafehuelendb_user:Lwn8nHS2yNEDEgrWjXY24MoNGEk4NWEX@dpg-cgpmf8d269v5rjftnov0-a.ohio-postgres.render.com/cafehuelendb',
+    conn_max_age=600
+    )
 }
+
 
 
 # Password validation
